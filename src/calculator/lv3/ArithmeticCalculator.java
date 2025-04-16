58a6ebf4c0d88af6f412dcd854ea50cf9b7eb217
ParkJin0814 <114732394+ParkJin0814@users.noreply.github.com>
2025-04-16 16:27:28 +0900

    문제발생 제너릭 변환과정에서 계산처리를 어떻게 할지 고민

--- src/calculator/lv3/ArithmeticCalculator.java
+++ src/calculator/lv3/ArithmeticCalculator.java
@@ -2,6 +2,42 @@ package calculator.lv3;
 
 import java.util.ArrayList;
 
-public class ArithmeticCalculator<T> {
+public class ArithmeticCalculator<T extends Number> {
+    private ArrayList<T> results = new ArrayList<>();
 
+    public ArrayList<T> getResults() {
+        return this.results;
+    }
+    public void setResults(ArrayList<T> results){
+        this.results = results;
+    }
+
+    public T makeCalculator (T a, T b, char arithmetic){
+        /*double result = 0;
+
+        switch (arithmetic) {
+            case '+':
+                result = OperatorType.addition.apply(a.doubleValue(), b.doubleValue());
+                break;
+            case '-':
+                result = OperatorType.subtract.apply(a, b);
+                break;
+            case '/':
+                result = OperatorType.divide.apply(a, b);
+                break;
+            case '*':
+                result = OperatorType.multiplication.apply(a, b);
+                break;
+            default:
+                System.out.println("잘못된 접근입니다.");
+                break;
+        }
+
+        return result;*/
+        return null;
+    }
+
+    public void removeFirstResult(){
+        results.removeFirst();
+    }
 }
