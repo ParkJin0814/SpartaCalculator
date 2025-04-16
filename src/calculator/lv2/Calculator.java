29817b3decdb3f9ae0ad9d6228d6e17efadc9b84
ParkJin0814 <114732394+ParkJin0814@users.noreply.github.com>
2025-04-16 13:40:46 +0900

    Calculatro class 생성 및 사칙연산 메서드구현 및 연산결과 저장배열추가

--- src/calculator/lv2/Calculator.java
+++ src/calculator/lv2/Calculator.java
@@ -1,4 +1,35 @@
 package calculator.lv2;
 
+import java.util.ArrayList;
+
 public class Calculator {
+    ArrayList<Integer> results = new ArrayList<>();
+
+    public int makeCalculator (int a, int b, char arithmetic){
+        int result = 0;
+        switch (arithmetic) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '/':
+                if(b == 0){
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                } else {
+                    result = a / b;
+                }
+                break;
+            case '*':
+                result = a * b;
+                break;
+            default:
+                System.out.println("잘못된 접근입니다.");
+                break;
+        }
+
+        results.add(result);
+        return result;
+    }
 }
