531b5380657adbed25b11ba7ad617ac7ba6f46b6
ParkJin0814 <114732394+ParkJin0814@users.noreply.github.com>
2025-04-23 20:45:09 +0900

    문제 주석 제거 및 정수뿐이아닌 소수도 받을 수 있도록 수정

--- src/calculator/lv3/App.java
+++ src/calculator/lv3/App.java
@@ -5,37 +5,21 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        // 1. 현재 사칙연산 계산기는 (➕,➖,✖️,➗) 이렇게 총 4가지 연산 타입으로 구성되어 있습니다.
-        // Enum 타입을 활용하여 연산자 타입에 대한 정보를 관리하고 이를 사칙연산 계산기 ArithmeticCalculator 클래스에 활용 해봅니다.
-
-        // 2. 실수, 즉 double 타입의 값을 전달 받아도 연산이 수행하도록 만들기
-        // 단순히, 기존의 Int 타입을 double 타입으로 바꾸는 게 아닌 점에 주의하세요!
-        // 지금까지는 ArithmeticCalculator, 즉 사칙연산 계산기는 양의 정수(0 포함)를 매개변수로 전달받아 연산을 수행
-        // 피연산자를 여러 타입으로 받을 수 있도록 기능을 확장
-        //ArithmeticCalculator 클래스의 연산 메서드(calculate)
-        // 위 요구사항을 만족할 수 있도록 ArithmeticCalculator 클래스를 수정합니다. (제네릭)
-        //추가적으로 수정이 필요한 다른 클래스나 메서드가 있다면 같이 수정 해주세요.
-
-        // 3. 저장된 연산 결과들 중 Scanner로 입력받은 값보다 큰 결과값 들을 출력
-        // ArithmeticCalculator 클래스에 위 요구사항을 만족하는 조회 메서드를 구현합니다.
-        // 단, 해당 메서드를 구현할 때 Lambda & Stream을 활용하여 구현합니다
-        // Java 강의에서 람다 & 스트림을 학습 및 복습 하시고 적용 해보세요!
-        // 추가) 람다 & 스트림 학습을 위해 여러 가지 조회 조건들을 추가하여 구현 해보시면 학습에 많은 도움이 되실 수 있습니다.
         Scanner sc = new Scanner(System.in);
-        ArithmeticCalculator<Integer> intCalculator = new ArithmeticCalculator<>();
+        ArithmeticCalculator<Number> intCalculator = new ArithmeticCalculator<>();
 
         while (true) {
-            int num1 = inputPositiveInteger(sc, "첫 번째 양의정수를 입력해주세요 : ");
-            int num2 = inputPositiveInteger(sc, "두 번째 양의정수를 입력해주세요 : ");
+            Number num1 = inputPositiveNumber(sc, "첫 번째 양의정수를 입력해주세요 : ");
+            Number num2 = inputPositiveNumber(sc, "두 번째 양의정수를 입력해주세요 : ");
             char arithmetic = inputArithmetic(sc);
 
-            if(arithmetic == '/' && num2 == 0){
+            if(arithmetic == '/' && num2.doubleValue() == 0){
                 System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                 continue;
             }
 
-            int result = intCalculator.calculate(num1, num2, arithmetic);
-            List<Integer> tempArray = intCalculator.getResults();
+            Number result = intCalculator.calculate(num1, num2, arithmetic);
+            List<Number> tempArray = intCalculator.getResults();
             tempArray.add(result);
             intCalculator.setResults(tempArray);
 
@@ -68,19 +52,23 @@ public class App {
         }
     }
 
-    public static int inputPositiveInteger(Scanner sc, String printText) {
-        int num = -1 ;
-        while (num < 0) {
+    public static Number inputPositiveNumber(Scanner sc, String printText) {
+        Number number = null;
+        while (true) {
             System.out.print(printText);
             String text = sc.nextLine();
-            if(text != null && !text.isEmpty() && text.chars().allMatch(Character::isDigit)){
-                num = Integer.parseInt(text);
-                if(num >= 0) break;
+            try {
+                number = Double.parseDouble(text);
+                if(number.doubleValue() > 0){
+                    break;
+                }
+            } catch (Exception e){
+                System.out.println("잘못된 값을 입력하였습니다.");
             }
 
             System.out.println("잘못된 값을 입력하였습니다.");
         }
-        return num;
+        return number;
     }
     
     public static char inputArithmetic(Scanner sc) {
